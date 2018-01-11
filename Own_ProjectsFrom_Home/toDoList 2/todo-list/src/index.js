7658606d8fb417098f56640691b62e7ae1864000
ashleynicole472 <ashleytaylor@Ashleys-Air.attlocal.net>
2018-01-10 21:49:12 -0800

    own proj

---
+++ Own_ProjectsFrom_Home/toDoList 2/todo-list/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import TodoList from './TodoList.js';
+
+var destination = document.querySelector("#container");
+
+ReactDOM.render(
+    <div>
+        <TodoList />
+    </div>,
+    destination
+);
