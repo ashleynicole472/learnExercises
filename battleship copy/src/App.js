54075df4663285855b7ac268099f2c98ef7e6f03
ashleynicole472 <asha.4720@gmail.com>
2018-01-23 12:23:56 -0800

    refactored code at school && some other stuff

--- battleship copy/src/App.js
+++ battleship copy/src/App.js
@@ -1,35 +1,96 @@
 import React, { Component } from 'react';
 
+const EMPTY = 0
+const SHIP = 1
+const HIT = 2
+const MISS = 3
+
 class Board extends Component {
   constructor(props){
     super(props)
     //Maintain state for the entire game in your main component
     this.state = {
-      board: this.getBoard(),
-      SHIP: 1,
+      board: this.getBoard(10),
+      shots: 25,
+      message: 'Take the shot!!!'
     }
+
     for(let i = 0;i < 5; i++){
       this.putShip()
     }
   }
 
   //loop create another for loop to create the <td> tags
-  renderRow(rowNumber){
-    let row = [];
-    for(let i = 0;i < 10; i++){
+  renderRow(rowNumber) {
+    const { board } = this.state
+
+    let row = []
+
+    for(let i = 0;i < 10; i++) {
       // create single cell with id of coordinates
       // 0-0   3-5
-      row.push(<td id={i + '-' +rowNumber}></td>);
+      let className;
+
+      switch (board[i][rowNumber]) {
+        case MISS:
+          className = 'miss'
+          break;
+        case HIT:
+          className = 'hit'
+          break;
+        case SHIP:
+          className = 'ship'
+          break;
+        default:
+          className = 'blank'
+      }
+
+      row.push(<td
+                  id={i + '-' +rowNumber}
+                  className={className}
+                  onClick={this.handleClick.bind(this)}>
+                  </td>)
+
     }
     return row;
   }
+
 //Use a for loop to append to a table a <tr> tag
   renderRows(){
     let newBoard = []
     for(let i = 0; i < 10; i++) {
       newBoard.push(<tr>{this.renderRow(i)}</tr>)
     }
+    console.log(this.state.board);
     return newBoard;
+  }
+//checks for ship on click, keeps track of shots taken
+  handleClick(event) {
+    var clickEvent = event.target.id;
+    var pos = clickEvent.split("-");
+    var x = pos[0];
+    var y = pos[1];
+    let newBoard = this.state.board;
+    let newShots = this.state.shots;
+    let newMessage = this.state.message;
+    if (newBoard[x][y] === 0) {
+      newBoard[x][y] = MISS;
+      newShots--;
+      newMessage = 'You missed!';
+    }else if (newBoard[x][y] === 1) {
+      newBoard[x][y] = HIT;
+      newShots--;
+      newMessage = 'You hit something!'
+    }
+    this.setState({
+      board: newBoard,
+      shots: newShots,
+      message: newMessage,
+    })
+
+    console.log(x + ' ' + y);
+    console.log("click")
+    console.log(newShots);
   }
 
   //and places 5 single length ships.
@@ -40,18 +101,24 @@ class Board extends Component {
     if(newBoard[a][b] === 1){
       this.putShip()
     } else{
-      newBoard[a][b] = this.state.SHIP
+      newBoard[a][b] = SHIP
       this.setState({board: newBoard});
     }
-    console.log(newBoard);
   }
 
-  getBoard() {
-    let newBoard = []
-    for (let i = 0 ; i < 10 ; i++) {
-      newBoard.push([0,0,0,0,0,0,0,0,0,0])
+  getBoard(boardSize) {
+    let board = []
+
+    for (let row = 0 ; row < boardSize; row++) {
+      board[row] = []
+      for (let col = 0 ; col < boardSize; col++) {
+        board[row][col] = EMPTY
+      }
     }
-    return newBoard;
+
+    // console.log(board);
+
+    return board;
   }
   // renderState(){
   //   this.getBoard();
@@ -61,11 +128,15 @@ class Board extends Component {
 
   render() {
     return (
-      <table>
-        <tbody>
-          { this.renderRows() }
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <tbody>
+            { this.renderRows() }
+          </tbody>
+        </table>
+        <h1 className = "msg">{this.state.message}</h1>
+        <span className = "shotCount">{this.state.shots}</span> shots left
+      </div>
     );
   }
 }
